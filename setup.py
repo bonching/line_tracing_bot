2659e460ae9a747fd53cd0a5ebe6d1b3e76bb547
Rafael Jay Ramores <rafael.p.ramores@gmail.com>
2025-08-10 07:20:34 +0000

    xacro

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name + '/launch', ['launch/display.launch.py']),
+        ('share/' + package_name + '/urdf', ['urdf/common_properties.xacro']),
+        ('share/' + package_name + '/urdf', ['urdf/mobile_base.xacro']),
         ('share/' + package_name + '/urdf', ['urdf/robot.urdf.xacro']),
         ('share/' + package_name + '/rviz', ['rviz/urdf_config.rviz']),
     ],
